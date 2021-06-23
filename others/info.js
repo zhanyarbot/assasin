02d8d4ecb277bc5ed6d710c0076e7bbdd2362f99
mirannnn <85360194+mirannnn@users.noreply.github.com>
2021-06-24 01:45:15 +0300

    Create info.js

---
+++ others/info.js
@@ -0,0 +1,35 @@
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const { 
+  PREFIX, 
+} = require(`../config.json`);
+
+  
+
+
+module.exports = {
+  name: "info",
+  aliases: ["INFO"],
+  cooldown: 8,
+  description: "**info commands**",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+    //.setThumbnail(`https://cdn.discordapp.com/avatars/837016151795564544/6813aeeea7e579ff5666bc9e902cb4c2.png?size=1024 `)
+    .setTitle(`**Qeqe Info**`)
+    .setDescription(`
+
+\`invite\` - \`support\` - \`about\`
+\`ping\` - \`prefix\` \`uptime\`
+
+
+[Support](https://discord.gg/6MGtNxJNBk)
+[Invite](https://discord.com/api/oauth2/authorize?client_id=837016151795564544&permissions=8&scope=bot)
+`)
+   .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+   .setColor("PURPLE");
+   message.react("<:emoji_4:822203026776391711>")
+    return message.channel.send(helpEmbed).catch(console.error);
+
+  }
+};
