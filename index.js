180431e83a11b7d63ae0c636000ba740c8866421
mirannnn <85360194+mirannnn@users.noreply.github.com>
2021-06-24 01:53:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ client.on(`ready`, () => {
         { return member.voice.channel.leave(); }
       });
       
-    client.user.setActivity(`Type: ${PREFIX}help - Rayal/Music`, { type: "PLAYING"});
+    client.user.setActivity(`Type: ${PREFIX}help - Qeqe`, { type: "PLAYING"});
     client.user.setActivity(`Type: ${PREFIX}help | ${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "PLAYING"});
    
   
