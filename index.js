ade324096b30f86c648d5615ef46e38984c1dab8
mirannnn <85360194+mirannnn@users.noreply.github.com>
2021-06-24 19:33:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -242,12 +242,12 @@ Online
     message.react("<:emoji_4:822203026776391711>")
   }
 
-client.on("guildCreate" , AQUAMAN => {
-  if(AQUAMAN.memberCount < 200){
-    console.log(`  name ( ${AQUAMAN.name} ) zhmaray memberakan ( ${AQUAMAN.memberCount}) created by AQUAMAN`)//by AQUAMAN
-    AQUAMAN.leave();
-  }
-})
+//client.on("guildCreate" , AQUAMAN => {
+ // if(AQUAMAN.memberCount < 200){
+   // console.log(`  name ( ${AQUAMAN.name} ) zhmaray memberakan ( ${AQUAMAN.memberCount}) created by AQUAMAN`)//by AQUAMAN
+  //  AQUAMAN.leave();
+//  }
+//})
 
   //An embed announcement for everyone but no one knows so fine ^w^
   if(message.content.startsWith(`${prefix}embed`)){
