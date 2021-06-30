6867b73abda0a0838579f6bb7a2f69355f7b3e03
mirannnn <85360194+mirannnn@users.noreply.github.com>
2021-06-30 13:14:21 +0300

    Delete say.js

--- others/say.js
+++
@@ -1,8 +0,0 @@
-client.on('message',function(message) {
- 
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(PREFIX + "say")) {
-if(!args) return;
-message.channel.send(`**# ${args}**`);
-}
-});
