b5f44d893dc265ddf96e7eaa7d7ea2e51c199f77
mirannnn <85360194+mirannnn@users.noreply.github.com>
2021-07-05 17:14:31 +0300

    Update info.js

--- others/info.js
+++ others/info.js
@@ -16,7 +16,7 @@ module.exports = {
 
     let helpEmbed = new MessageEmbed()
     //.setThumbnail(`https://cdn.discordapp.com/avatars/837016151795564544/6813aeeea7e579ff5666bc9e902cb4c2.png?size=1024 `)
-    .setTitle(`**Qeqe Info**`)
+    .setTitle(`**Nito Info**`)
     .setDescription(`
 
 >>> \`invite\` - \`support\` - \`about\`
