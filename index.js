c29d13f03ff20af8ea514868b89e2f99f218aba2
mirannnn <85360194+mirannnn@users.noreply.github.com>
2021-07-12 17:05:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ client.on('message',function(message) {
 let args = message.content.split(" ").slice(1).join(" ");
 if(message.content.startsWith(PREFIX + "say")) {
 if(!args) return;
-message.channel.send(`**# ${args}**`);
+message.channel.send(`** ${args}**`);
 }
 });
 
