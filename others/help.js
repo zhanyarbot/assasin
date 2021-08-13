f74ec30c2f5e4ba6df5278c92fa52712d6fe2f2c
Omed99099 <81825131+Omed99099@users.noreply.github.com>
2021-08-13 04:52:17 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -20,30 +20,30 @@ module.exports = {
     .setDescription(`
     
 **__Moderator__**
-<a<a:emoji_30:861768375792566272>861768375792566272>\`,lock\`  -  \`,unlock\`  -  \`,clear\`  -  \`,kick\`
-<a<a:emoji_30:861768375792566272>861768375792566272>\`,ban\`  -  \`,unban\`  -  \`,sug\`
+<a:emoji_30:861768375792566272>\`,lock\`  -  \`,unlock\`  -  \`,clear\`  -  \`,kick\`
+<a:emoji_30:861768375792566272>\`,ban\`  -  \`,unban\`  -  \`,sug\`
 
 
 **__Funny__**
-<a<a:emoji_83:875554071631757385>875554071631757385>\`,slap\`  -   \`,kiss\`  -  \`,hug\`
+<a:emoji_83:875554071631757385>\`,slap\`  -   \`,kiss\`  -  \`,hug\`
 
 
 **__Gif__**
-<a<a:emoji_26:861768220776333362>861768220776333362>\`,boy\`   -   \`,girl\`   -  \`,baby\`  -  \`,couple\`
-<a<a:emoji_26:861768220776333362>861768220776333362>\`,anime\`  -  \`,cat\`  -  \`,cartoon\`  -  \`,smoke\`
-<a<a:emoji_26:861768220776333362>861768220776333362>\`,sad\`  -  \`,neon\`
+<a:emoji_26:861768220776333362>\`,boy\`   -   \`,girl\`   -  \`,baby\`  -  \`,couple\`
+<a:emoji_26:861768220776333362>\`,anime\`  -  \`,cat\`  -  \`,cartoon\`  -  \`,smoke\`
+<a:emoji_26:861768220776333362>\`,sad\`  -  \`,neon\`
 
 
 **__Music__**
-<a<a:emoji_82:875552188749983815>875552188749983815>\`,play\`  -  \`,skip\`  -  \`,skipto\`  -  \`,stop\`  
-<a<a:emoji_82:875552188749983815>875552188749983815>\`,volume\`  -  \`,nowplaying\`  -  \`,shuffle\`
-<a<a:emoji_82:875552188749983815>875552188749983815>\`,search\`  -  \`,resume\`  -  \`,remove\`
-<a<a:emoji_82:875552188749983815>875552188749983815>\`,queue\`  -  \`,loop\`  -  \`,lyrics\`  -  \`,radio\`
+<a:emoji_82:875552188749983815>\`,play\`  -  \`,skip\`  -  \`,skipto\`  -  \`,stop\`  
+<a:emoji_82:875552188749983815>\`,volume\`  -  \`,nowplaying\`  -  \`,shuffle\`
+<a:emoji_82:875552188749983815>\`,search\`  -  \`,resume\`  -  \`,remove\`
+<a:emoji_82:875552188749983815>\`,queue\`  -  \`,loop\`  -  \`,lyrics\`  -  \`,radio\`
 
 
 **__Everyone__**
-<a<a:emoji_74:865927465377202196>865927465377202196>\`,invite\`  -  \`,support\`  -  \`,about\`  -  \`,ping\`
-<a<a:emoji_74:865927465377202196>865927465377202196>\`,prefix\`  -  \`,uptime\`
+<a:emoji_74:865927465377202196>\`,invite\`  -  \`,support\`  -  \`,about\`  -  \`,ping\`
+<a:emoji_74:865927465377202196>\`,prefix\`  -  \`,uptime\`
 
 
 [Support](https://discord.gg/DCYsfe4AR6)
