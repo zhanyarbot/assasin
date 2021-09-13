0b30202e716eed56230840f9e6f4fead6ca3663a
Omed99099 <81825131+Omed99099@users.noreply.github.com>
2021-09-14 01:35:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -612,7 +612,7 @@ client.on(`ready`, () => {
         { return member.voice.channel.leave(); }
       });
       
-    client.user.setActivity(`Type: ${PREFIX}help - Nito`, { type: "PLAYING"});
+    client.user.setActivity(`Type: ${PREFIX}help - Cupe`, { type: "PLAYING"});
     client.user.setActivity(`Type: ${PREFIX}help | ${client.guilds.cache.size} Server,Users ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)},`, { type: "PLAYING"});
    
   
