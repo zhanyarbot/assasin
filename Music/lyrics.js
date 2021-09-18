7488d92e16ae96cb254b83fc8b4d1b01c83bb039
Omed99099 <81825131+Omed99099@users.noreply.github.com>
2021-09-18 15:05:35 +0300

    Update lyrics.js

--- Music/lyrics.js
+++ Music/lyrics.js
@@ -50,7 +50,7 @@ async execute(message) {
     let lyricsEmbed = new MessageEmbed()
       .setTitle("🌐 Lyrics")
       .setDescription(lyrics)
-      .setColor("PURPLE")
+      .setColor("GREEN")
     //if to long make slice it
     if (lyricsEmbed.description.length >= 2048)
       //slice the embed description and redefine it
