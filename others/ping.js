c00de9b3894afaa0022f058c1c54a1d1c52c2617
Omed99099 <81825131+Omed99099@users.noreply.github.com>
2021-09-18 14:57:42 +0300

    Update ping.js

--- others/ping.js
+++ others/ping.js
@@ -13,8 +13,8 @@ module.exports = {
   edesc: "Type this command to see how fast the Bot can response to your messages / commands inputs!",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("<:emoji_4:822203026776391711>");
+    message.react("📶");
     //send the Ping embed
-    message.reply(new MessageEmbed().setColor("PURPLE").setTitle("📶 `" + client.ws.ping + "ms`"));
+    message.reply(new MessageEmbed().setColor("GREEN").setTitle("📶 `" + client.ws.ping + "ms`"));
   }
 }
