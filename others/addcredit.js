03cc8dc5deee0530644592a7b25cfff1af3f232e
Omed99099 <81825131+Omed99099@users.noreply.github.com>
2021-10-04 02:36:11 +0300

    Create addcredit.js

---
+++ others/addcredit.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const db = require('quick.db');
+module.exports = {
+    name:"addcredits",
+     run: async(client, message, args) => {
+        const ownerId = "حط ايديك هني" // this is my id but you wanna put your id here.
+
+        if(message.author.id !== ownerId) {
+            return message.channel.send(`Sorry! Only the bot developer can use this command.`)
+        }
+        let user = message.mentions.users.first()
+        if( !args[0] || !user) {
+            return message.channel.send(`The first argument must be an user mention!`)
+        }
+        let amount = args.slice(1).join(" ");
+        if(!amount) {
+            return message.channel.send(`Please provide a number!`)
+        }
+        if(isNaN(amount)) {
+            return message.channel.send(`That's not a number!`)
+        }
+        if(amount === "0")  {
+            return message.channel.send(`Why'd you give someone \`0\` credit??? 😂😂`)
+        }
+        db.add(`credit_${user.id}`, parseInt(amount))
+        message.channel.send(`Succesfully added \`${parseInt(amount)}\` credits to **${user.tag}**'s !`)
+
+    }
+}
+//Made by Привет ⁞ 💘ֆ ⁽ʜɪᴇ ⁞ ⁾↵⇣℡#2000
