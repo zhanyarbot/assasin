900b7bc0f12c02b25abcb4b47f78b3a0114eb82b
Omed99099 <81825131+Omed99099@users.noreply.github.com>
2021-10-04 02:54:39 +0300

    Update attentionembed.js

--- util/attentionembed.js
+++ util/attentionembed.js
@@ -16,11 +16,11 @@ module.exports = {
         }
 
     let resultsEmbed = new MessageEmbed()
-      .setTitle("<:emoji_4:822203026776391711>" + titel)
-      .setColor("PURPLE")
+      .setTitle("" + titel)
+      .setColor("GREEN")
       
       message.channel.send(resultsEmbed);
-      message.react("<:emoji_4:822203026776391711>")
+      message.react("💚")
     return;
 
   }
